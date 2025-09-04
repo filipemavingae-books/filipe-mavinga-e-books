c2002383157511ddac9804a102e1a184a2c67745
filipemavingae-books <filipemavingaebooks@gmail.com>
2025-09-05 00:53:45 +0100

    Update server.ts with new Supabase client code

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,14 +1,15 @@
-import { createServerClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
+import { createServerClient } from "@supabase/ssr";
+import cookies from "next-cookies";
 
-export function createClient() {
+export function createClient(ctx: any) {
+  const cookieStore = cookies(ctx);
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
-          return cookies().get(name)?.value;
+          return cookieStore[name];
         },
       },
     }
