c8781e247463b0753fa4f100c315a61c2621d4b7
filipemavingae-books <filipemavingaebooks@gmail.com>
2025-09-05 01:17:52 +0100

    Update server.ts
    Signed-off-by: filipemavingae-books <filipemavingaebooks@gmail.com>

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,25 +1,14 @@
 import { createServerClient } from "@supabase/ssr";
-import { cookies } from "next/headers";
+import type { GetServerSidePropsContext } from "next";
 
-export async function createClient() {
-  const cookieStore = await cookies();
-
+export function createClient(ctx: GetServerSidePropsContext) {
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll() {
-          return cookieStore.getAll();
-        },
-        setAll(cookiesToSet) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            );
-          } catch {
-            // Quando chamado a partir de um Server Component, isso pode ser ignorado
-          }
+        get(name: string) {
+          return ctx.req.cookies[name];
         },
       },
     }
